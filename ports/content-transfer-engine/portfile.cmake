a9db7cb93afa5b8ca8ef01c2cd16fc87ef1af520
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2025-06-09 16:43:56 -0500

    build(vcpkg): update hash for cte

--- ports/content-transfer-engine/portfile.cmake
+++ ports/content-transfer-engine/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO iowarp/content-transfer-engine
-    REF "bab57b6e2d106a829ed1e3c0ddee37aed2132d38"
+    REF "f1de9307efae2a3f863c1555439bd15fc8654222"
     SHA512 46d23501e09522fc28b7f730f8e9836ee569f80b6d6c2a7944343361b5291c954285124c5354b8bdf814d00293a21ebd2a2064c345237bcf79a7e926e8ac30c5    
     HEAD_REF main
 )
