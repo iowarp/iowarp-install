c2f6b21c934cc1d3bacf29cd9a1e003044bc6d37
H. Joe Lee <hyoklee@hdfgroup.org>
2025-06-23 14:42:48 -0500

    test(vcpkg): 2025.06.13

--- build.bat
+++ build.bat
@@ -4,4 +4,5 @@ cd vcpkg
 call bootstrap-vcpkg.bat
 .\vcpkg install cte-hermes-shm[core]
 .\vcpkg install iowarp-runtime[core]
+.\vcpkg install content-assimilation-engine[poco,s3]
 
--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-git clone  --branch 2025.03.19 --single-branch https://github.com/microsoft/vcpkg
+git clone  --branch 2025.06.13 --single-branch https://github.com/microsoft/vcpkg
 cp -r ./ports/* ./vcpkg/ports/
 cd vcpkg
 ./bootstrap-vcpkg.sh
 ./vcpkg install content-transfer-engine
+
