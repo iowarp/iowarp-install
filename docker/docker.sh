74b8fd42e17a26217e552392e321560eb6b0e988
lukemartinlogan <lukemartinlogan@gmail.com>
2025-11-01 09:51:41 -0500

    Rename the workflow actions

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,9 +1,10 @@
-docker build --no-cache -t iowarp-base docker -f docker/iowarp-base.Dockerfile
+#!/bin/bash
+docker build --no-cache -t iowarp-base -f iowarp-base.Dockerfile .
 docker tag iowarp-base iowarp/iowarp-base:latest
 # docker push iowarp/iowarp-base:latest
-docker build --no-cache -t iowarp-deps docker -f docker/iowarp-deps.Dockerfile
+docker build --no-cache -t iowarp-deps -f iowarp-deps.Dockerfile .
 docker tag iowarp-deps iowarp/iowarp-deps:latest
 # docker push iowarp/iowarp-deps:latest
-docker build --no-cache -t iowarp docker -f docker/iowarp.Dockerfile
+docker build --no-cache -t iowarp -f iowarp.Dockerfile .
 docker tag iowarp iowarp/iowarp:latest
 # docker push iowarp/iowarp:latest
